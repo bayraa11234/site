0aedf88a69db91261819fc126157b0084370645c
odbayar <odbayardorjsuren8@gmail.com>
2023-03-21 18:56:19 +0800

    apdate

--- src/Nabvar/LittleNavbar.jsx
+++ src/Nabvar/LittleNavbar.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import { CiLocationOn } from "react-icons/ci";
+import React from "react";
+import { CiLocationOn, CiAlarmOn, CiPhone, CiMail } from "react-icons/ci";
 
 export default function LittleNavbar() {
   return (
-    <div>
-        <div>
-        <CiLocationOn fontSize={50} color="#00a199"/>
-        </div>
+    <div style={{ backgroundColor: "rgba(0, 161, 153,0.2)", display: "flex" }}>
+      <div>
+        <CiLocationOn fontSize={50} color="#00a199" />
+      </div>
+      <div>
+        <CiAlarmOn fontSize={50} color="#00a199" />
+      </div>
+      <div>
+        <CiMail fontSize={50} color="#00a199" />
+      </div>
+      <div>
+        <CiPhone fontSize={50} color="#00a199" />
+      </div>
     </div>
-  )
+  );
 }
